5bb7b3b505c247710b0558d070bf8da307573e03
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-11-03 21:01:41 +0700

    Init model, database and controller

---
+++ app/Models/Time.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Time extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'checkin',
+        'checkout',
+        'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2022_11_03_133529_create_day_offs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDayOffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('day_offs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id");
+            $table->enum("shifts", [1, 2, 3]); // 1: ca sáng, 2: ca chiều, 3: cả ngày
+            $table->date("date");
+            $table->enum("status", [0, 1, 2])->default(0); // 0: đang đợi duyệt, 1: chấp nhận, 2: từ chối
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('day_offs');
+    }
+}
